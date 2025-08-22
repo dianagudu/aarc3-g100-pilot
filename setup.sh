fdf8ed8b9ac585f3eb486cbcbe7094b2f6828055
Diana Gudu <diana.gudu@kit.edu>
2025-08-22 15:51:16 +0000

    fix rp non existent data dir

--- setup.sh
+++ setup.sh
@@ -3,6 +3,13 @@
 docker network create caddy
 docker compose up -d
 
+sleep 20
+
+curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://tmi-sirtfi.g100.aarc3-pilots.vm.fedcloud.eu
+
+curl https://ta1.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp.g100.aarc3-pilots.vm.fedcloud.eu
+curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp.g100.aarc3-pilots.vm.fedcloud.eu
+
 docker run --rm \
   --user "$(id -u):$(id -g)" \
   -v "${PWD}/rp-expl:/conf" \
@@ -10,11 +17,6 @@ docker run --rm \
   --entrypoint python3 \
   ddgu/fedservice \
   /app/setup.py /conf
-
-curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://tmi-sirtfi.g100.aarc3-pilots.vm.fedcloud.eu
-
-curl https://ta1.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp.g100.aarc3-pilots.vm.fedcloud.eu
-curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp.g100.aarc3-pilots.vm.fedcloud.eu
 
 curl https://ta1.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp-expl.g100.aarc3-pilots.vm.fedcloud.eu
 curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp-expl.g100.aarc3-pilots.vm.fedcloud.eu
