be0b62abc46363a23a238e08556bc4920fc6b810
Diana Gudu <diana.gudu@kit.edu>
2025-08-24 21:40:54 +0000

    added fedservice op

--- setup.sh
+++ setup.sh
@@ -5,12 +5,24 @@ docker compose up -d
 
 sleep 60
 
+####################
+# set up tmi-sirtfi
+####################
+
 curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://tmi-sirtfi.g100.aarc3-pilots.vm.fedcloud.eu
+
+####################
+# set up rp
+####################
 
 sleep 5
 docker compose restart rp
 curl https://ta1.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp.g100.aarc3-pilots.vm.fedcloud.eu
 curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp.g100.aarc3-pilots.vm.fedcloud.eu
+
+####################
+# set up rp-expl
+####################
 
 docker run --rm \
   --user "$(id -u):$(id -g)" \
@@ -29,3 +41,25 @@ docker compose restart rp-expl
 sleep 5
 curl https://ta1.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp-expl.g100.aarc3-pilots.vm.fedcloud.eu
 curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://rp-expl.g100.aarc3-pilots.vm.fedcloud.eu
+
+####################
+# set up OP
+####################
+
+docker run --rm \
+  --user "$(id -u):$(id -g)" \
+  -v "${PWD}/op:/conf" \
+  --network caddy \
+  --entrypoint python3 \
+  ddgu/fedservice \
+  /app/setup.py /conf
+
+TM=`curl https://tmi-sirtfi.g100.aarc3-pilots.vm.fedcloud.eu/trustmark\?sub\=https://op.g100.aarc3-pilots.vm.fedcloud.eu\&trust_mark_type\=https://refeds.org/sirtfi`
+
+echo "[{\"trust_mark\": \"${TM}\", \"trust_mark_type\": \"https://refeds.org/sirtfi\"}]" > op/op/op/trust_marks
+
+docker compose restart op
+
+sleep 5
+curl https://ta1.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://op.g100.aarc3-pilots.vm.fedcloud.eu
+curl https://ta2.g100.aarc3-pilots.vm.fedcloud.eu/enroll?sub=https://op.g100.aarc3-pilots.vm.fedcloud.eu
